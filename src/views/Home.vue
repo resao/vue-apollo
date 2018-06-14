2e9aabf7c3f03a9ca68d249cb3e15c2b64435f15
Christopher Miles <mail@christophermiles.co.uk>
2018-06-14 18:00:32 +0100

    Access store from view to render our fruit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if="fruits">
+      <li v-for="fruit in fruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
+    </ul>
   </div>
 </template>
 
@@ -13,6 +16,14 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  computed: {
+    fruits () {
+      return this.$store.state.fruits
+    }
+  },
+  created () {
+    this.$store.dispatch('getFruits')
   }
 }
 </script>
