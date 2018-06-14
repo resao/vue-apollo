a7b931a4483e4521f049b469bb56913557d99c80
Christopher Miles <mail@christophermiles.co.uk>
2018-06-14 20:01:36 +0100

    Moved fruit list into separate component

---
+++ src/components/FruitList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul v-if="fruits">
+    <li v-for="fruit in fruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
+  </ul>
+</template>
+<script>
+
+import * as types from '@/store/modules/fruit/types'
+
+export default {
+  computed: {
+    fruits () {
+      return this.$store.getters.fruits
+    }
+  },
+  created () {
+    this.$store.dispatch(types.FETCH_FRUITS)
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <ul v-if="fruits">
-      <li v-for="fruit in fruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
-    </ul>
+    <app-fruit-list></app-fruit-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FruitList from '@/components/FruitList.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  },
-  computed: {
-    fruits () {
-      return this.$store.state.fruits
-    }
-  },
-  created () {
-    this.$store.dispatch('getFruits')
+    appFruitList: FruitList
   }
 }
 </script>
