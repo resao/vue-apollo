9b1d70b7823eefaf7afec70658cb6af0e17f58ed
Christopher Miles <mail@christophermiles.co.uk>
2018-06-15 16:12:21 +0100

    Visual tidy of fruit listing

--- src/components/FruitList.vue
+++ src/components/FruitList.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul v-if="fruits">
-    <li v-for="fruit in fruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
-  </ul>
+  <div class="all-fruits">
+    <h2>All Fruits</h2>
+    <ul v-if="fruits">
+      <li v-for="fruit in fruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
+    </ul>
+  </div>
 </template>
 <script>
 
