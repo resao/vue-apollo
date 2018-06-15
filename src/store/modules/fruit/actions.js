bca9bf5c96985cfc5ebbc13992c0c7cd62d26284
Christopher Miles <mail@christophermiles.co.uk>
2018-06-15 16:11:59 +0100

    Changed allFruit over to gql, added new fruitByName

--- src/store/modules/fruit/actions.js
+++ src/store/modules/fruit/actions.js
@@ -1,12 +1,23 @@
 import apolloClient from '@/apollo/index.js'
-import { queryAllFruit } from '@/apollo/queries/fruit.js'
+import allFruit from '@/graphql/allFruit.gql'
+import fruitByName from '@/graphql/fruitByName.gql'
 import * as types from './types'
 
 export default {
   [types.FETCH_FRUITS]: (context) => {
     apolloClient.query({
-      query: queryAllFruit
-    }).then(({data}) => {
+      query: allFruit
+    }).then(({ data }) => {
+      context.commit(types.MUTATE_FRUITS, data.allFruits)
+    })
+  },
+  [types.FETCH_FRUIT_BY_NAME]: (context, name) => {
+    apolloClient.query({
+      query: fruitByName,
+      variables: {
+        name: name
+      }
+    }).then(({ data }) => {
       context.commit(types.MUTATE_FRUITS, data.allFruits)
     })
   }
--- src/store/modules/fruit/types.js
+++ src/store/modules/fruit/types.js
@@ -3,3 +3,4 @@ export const MUTATE_FRUITS = 'fruit/MUTATE'
 
 // Actions
 export const FETCH_FRUITS = 'fruit/FETCH'
+export const FETCH_FRUIT_BY_NAME = 'fruit/FETCH_BY_NAME'
